660e8c266522c0a61e920d5b0b06d7f5d61ff04a
guzongjia <kabuto412rock@gmail.com>
2020-04-14 12:27:02 +0800

    可以正常顯示上一頁的按鈕＆添加頁面的轉移動畫

--- app/src/main/java/com/blogspot/zongjia/demomeow/MainActivity.kt
+++ app/src/main/java/com/blogspot/zongjia/demomeow/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.blogspot.zongjia.demomeow
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.GridLayoutManager
-import com.blogspot.zongjia.demomeow.R
-import com.blogspot.zongjia.demomeow.presentation.detail.DetailActivity
-import com.blogspot.zongjia.demomeow.presentation.main.adapter.CatAdapter
-import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.viewmodel.ext.android.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-
+//    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
     }
 }
