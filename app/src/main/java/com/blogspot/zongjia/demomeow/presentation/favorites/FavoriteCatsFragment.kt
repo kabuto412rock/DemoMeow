c552f5314bc4c30d422caa9f1ebf324eeb925cdc
guzongjia <kabuto412rock@gmail.com>
2020-04-19 11:08:37 +0800

    Add: 添加在收藏頁面可以向左滑刪除item(貓圖)，並且可以Undo上一個動作。不依賴其他函式庫

--- app/src/main/java/com/blogspot/zongjia/demomeow/presentation/favorites/FavoriteCatsFragment.kt
+++ app/src/main/java/com/blogspot/zongjia/demomeow/presentation/favorites/FavoriteCatsFragment.kt
@@ -1,9 +1,14 @@
 package com.blogspot.zongjia.demomeow.presentation.favorites
 
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
@@ -15,7 +20,6 @@ import com.blogspot.zongjia.demomeow.R
 import com.blogspot.zongjia.demomeow.data.entities.Cat
 import com.blogspot.zongjia.demomeow.presentation.favorites.adapter.FavoriteCatsAdapter
 import com.blogspot.zongjia.demomeow.presentation.favorites.dialogs.DialogClearConfirmFragment
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_favorite_cat.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -29,57 +33,6 @@ class FavoriteCatsFragment : Fragment() {
     private var menuActionClear: MenuItem? = null
     private lateinit var catAdapter : FavoriteCatsAdapter
     private var deleteCat: Cat? = null
-    private val simpleCallback =
-        object : ItemTouchHelper.SimpleCallback(
-            0, ItemTouchHelper.LEFT
-    ) {
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean {
-            return false
-        }
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            val position = viewHolder.adapterPosition
-            when(direction) {
-                // 當手指往左滑 <-<-
-                ItemTouchHelper.LEFT -> {
-                    viewModel.catsList.value?.get(position)?.also {cat ->
-                        deleteCat = cat
-                        viewModel.removeCat(cat)
-//                        catAdapter.notifyItemRemoved(position)
-                        Snackbar.make(catsRecyclerView, "你刪除了貓${cat.id}", Snackbar.LENGTH_LONG)
-                            .setAction("復原"){
-                                viewModel.insertCat(cat)
-                            }.show()
-                    }
-                }
-            }
-        }
-
-            override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                super.onChildDraw(
-                    c,
-                    recyclerView,
-                    viewHolder,
-                    dX,
-                    dY,
-                    actionState,
-                    isCurrentlyActive
-                )
-            }
-    }
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.favoiretes_menu, menu)
         menuActionClear = menu.findItem(R.id.favorites_menu_action_clear)
@@ -126,12 +79,12 @@ class FavoriteCatsFragment : Fragment() {
         val onCatClicked: (cat: Cat)-> Unit =  { cat->
             viewModel.catClicked(cat)
         }
-        // 初始化RecyclerView需要的SwipeItemHelper
-        val itemTouchHelper = ItemTouchHelper(simpleCallback)
-        itemTouchHelper.attachToRecyclerView(catsRecyclerView)
+        val onCatRemoved: (cat: Cat)-> Unit = {
+            viewModel.removeCat(it)
+        }
 
         // Instantiate our custom Adapter
-        catAdapter = FavoriteCatsAdapter(onCatClicked)
+        catAdapter = FavoriteCatsAdapter(onCatClicked, onCatRemoved)
         catsRecyclerView.apply {
             // Displaying data in a Grid design
             layoutManager = LinearLayoutManager(
@@ -139,8 +92,194 @@ class FavoriteCatsFragment : Fragment() {
             )
             adapter = catAdapter
         }
-
+        // 參考SwipItem作法:https://github.com/nemanja-kovacevic/recycler-view-swipe-to-delete/blob/master/app/src/main/java/net/nemanjakovacevic/recyclerviewswipetodelete/MainActivity.java
+        // 初始化RecyclerView需要的SwipeItemHelper
+        setUpItemTouchHelper()
+        // 初始化ItemAnimationDecorationHelper()
+        setUpAnimationDecoratorHelper()
     }
+    /**
+     * This is the standard support library way of implementing "swipe to delete" feature. You can do custom drawing in onChildDraw method
+     * but whatever you draw will disappear once the swipe is over, and while the items are animating to their new position the recycler view
+     * background will be visible. That is rarely an desired effect.
+    */
+    private fun setUpItemTouchHelper(){
+        val simpleItemTouchCallback = object :ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            lateinit var background: Drawable
+            lateinit var xMark: Drawable
+            var xMarkMargin: Int = 0
+            var initiated : Boolean = false
+            fun init() {
+                context?.apply {
+                    background = ColorDrawable(Color.RED)
+                    xMark = ContextCompat.getDrawable(this, R.drawable.ic_delete_white_24dp)!!
+                    xMark.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                    xMarkMargin = resources.getDimension(R.dimen.ic_clear_margin).toInt()
+                    initiated = true
+                }
+            }
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun getSwipeDirs(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val position = viewHolder.adapterPosition
+                val adapter = recyclerView.adapter as FavoriteCatsAdapter
+                if (adapter.isPendingRemoval(position)) {
+                    return 0
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder)
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val swipePosition = viewHolder.adapterPosition
+                val adapter = catsRecyclerView.adapter as FavoriteCatsAdapter
+                // Has undo on
+                adapter.pendingRemoval(swipePosition)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                val itemView = viewHolder.itemView
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.adapterPosition == -1) {
+                    return
+                }
+                if (!initiated) {
+                    init()
+                }
+                // draw red background
+                background.setBounds(itemView.right + dX.toInt(), itemView.top,itemView.right,itemView.bottom)
+                background.draw(c)
+
+                // draw TrashCan mark
+                val itemHeight = itemView.bottom - itemView.top
+                val intrinsicWidth = xMark.intrinsicWidth
+                val intrinsicHeight = xMark.intrinsicHeight
+
+                val xMarkLeft = itemView.right - xMarkMargin - intrinsicWidth
+                val xMarkRight = itemView.right - xMarkMargin
+                val xMarkTop = itemView.top + (itemHeight - intrinsicHeight) / 2
+                val xMarkBottom = xMarkTop + intrinsicHeight
+                xMark.setBounds(xMarkLeft, xMarkTop, xMarkRight, xMarkBottom)
+                xMark.draw(c)
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(catsRecyclerView)
+    }
+    /*
+    * We're gonna setup another ItemDecorator that will draw the red background in the empty space while the items are animating to thier new positions
+    * after an item is removed.
+    */
+    private fun setUpAnimationDecoratorHelper() {
+        catsRecyclerView.addItemDecoration(object: RecyclerView.ItemDecoration() {
+            // we want to cache this and not allocate anything repeatedly in the onDraw method
+            var background: Drawable? = null
+            var initiated: Boolean = false
+
+            private fun init() {
+                background = ColorDrawable(Color.RED)
+                initiated = true
+            }
+
+            override fun onDraw(c: Canvas ,parent: RecyclerView, state: RecyclerView.State ) {
+                if (!initiated) {
+                    init()
+                }
+
+                // only if animation is in progress
+                if (parent.itemAnimator?.isRunning() ?: false) {
+
+                    // some items might be animating down and some items might be animating up to close the gap left by the removed item
+                    // this is not exclusive, both movement can be happening at the same time
+                    // to reproduce this leave just enough items so the first one and the last one would be just a little off screen
+                    // then remove one from the middle
+
+                    // find first child with translationY > 0
+                    // and last one with translationY < 0
+                    // we're after a rect that is not covered in recycler-view views at this point in time
+                    var lastViewComingDown: View? = null
+                    var firstViewComingUp: View? =null
+
+                    // this is fixed
+                    val left: Int = 0
+                    val right = parent.width
+
+                    // this we need to find out
+                    var top = 0
+                    var bottom = 0
+
+                    // find relevant translating views
+                    val childCount = parent.layoutManager?.childCount
+                    val layoutManager = parent.getLayoutManager()
+                    if (childCount == null|| layoutManager == null) return
+                    for (i in 0 until childCount) {
+                        layoutManager?.getChildAt(i)?.also { child ->
+                            if (child.getTranslationY() < 0) {
+                                // view is coming down
+                                lastViewComingDown = child
+                            } else if (child.getTranslationY() > 0) {
+                                // view is coming up
+                                if (firstViewComingUp == null) {
+                                    firstViewComingUp = child;
+                                }
+                            }
+                        }
+                    }
+
+                    if (lastViewComingDown != null && firstViewComingUp != null) {
+                        // views are coming down AND going up to fill the void
+                        val lastDownB = lastViewComingDown?.bottom
+                        val lastDownTy = lastViewComingDown?.translationY?.toInt()
+                        val firstUpTop = firstViewComingUp?.top
+                        val firstUpTy = firstViewComingUp?.translationY?.toInt()
+                        if (lastDownB !=null && lastDownTy != null && firstUpTop != null && firstUpTy != null) {
+                            top = lastDownB + lastDownTy
+                            bottom = firstUpTop + firstUpTy
+                        }
+                    } else if (lastViewComingDown != null) {
+                        // views are going down to fill the void
+                        val downB = lastViewComingDown?.bottom
+                        val downTy = lastViewComingDown?.translationY?.toInt()
+                        if (downB !=null && downTy != null) {
+                            top = downB + downTy
+                            bottom = downB
+                        }
+                    } else if (firstViewComingUp != null) {
+                        // views are coming up to fill the void
+                        val firstUpTop = firstViewComingUp?.getTop()
+                        val firstUpTy = firstViewComingUp?.getTranslationY()?.toInt()
+                        if (firstUpTop != null && firstUpTy != null) {
+                            top = firstUpTop
+                            bottom = firstUpTop + firstUpTy
+                        }
+                    }
+                    background?.setBounds(left, top, right, bottom)
+                    background?.draw(c)
+
+                }
+                super.onDraw(c, parent, state)
+            }
+
+        })
+    }
+
     private fun initViewModel() {
         // 觀察catList和當取得新CatList回報時更新adapter
         viewModel.catsList.observe(viewLifecycleOwner, Observer { newCatsList ->
--- app/src/main/java/com/blogspot/zongjia/demomeow/presentation/favorites/adapter/FavoriteCatsAdapter.kt
+++ app/src/main/java/com/blogspot/zongjia/demomeow/presentation/favorites/adapter/FavoriteCatsAdapter.kt
@@ -1,5 +1,7 @@
 package com.blogspot.zongjia.demomeow.presentation.favorites.adapter
 
+import android.graphics.Color
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,18 +10,29 @@ import com.blogspot.zongjia.demomeow.R
 import com.blogspot.zongjia.demomeow.data.entities.Cat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import kotlinx.android.synthetic.main.item_cat.view.*
+import kotlinx.android.synthetic.main.item_swipe_cat.view.*
 import kotlin.properties.Delegates
 
-class FavoriteCatsAdapter(val onCatClicked: ((Cat) -> Unit)): RecyclerView.Adapter<FavoriteCatsAdapter.CatViewHolder>() {
+
+class FavoriteCatsAdapter(val onCatClicked:((Cat) -> Unit), val onCatRemoved: ((Cat) -> Unit)): RecyclerView.Adapter<FavoriteCatsAdapter.CatViewHolder>() {
+    companion object {
+        private val PENDING_REMOVAL_TIMEOUT: Long = 3000 // 3sec
+    }
+
     // Our data list is going to be notified when we assign a new list of data to it
     private var catList: List<Cat> by Delegates.observable(emptyList()) {
             _, _, _ -> notifyDataSetChanged()
     }
+    private var itemsPendingRemoval: MutableList<Cat> = emptyList<Cat>().toMutableList()
+    private val handler: Handler = Handler() // hanlder for running delayed runnables
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
+    var pendingRunnables: HashMap<Cat, Runnable> =
+        HashMap() // map of items to pending runnables, so we can cancel a removal if need be
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteCatsAdapter.CatViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_cat, parent, false)
+        val view = inflater.inflate(R.layout.item_swipe_cat, parent, false)
         val holder = CatViewHolder(view)
         holder.itemView.setOnClickListener {
             if (holder.adapterPosition != RecyclerView.NO_POSITION) {
@@ -32,9 +45,34 @@ class FavoriteCatsAdapter(val onCatClicked: ((Cat) -> Unit)): RecyclerView.Adapt
 
     override fun getItemCount(): Int = catList.size
 
-    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
-        if (position != RecyclerView.NO_POSITION) {
-            val cat = catList[position]
+    override fun onBindViewHolder(holder: FavoriteCatsAdapter.CatViewHolder, position: Int) {
+        if (position == RecyclerView.NO_POSITION) return
+
+        val cat = catList[position]
+//        holder.bind(cat)
+
+        if(itemsPendingRemoval.contains(cat)) {
+            // Show itemView in "show undo button" state
+            holder.itemView.setBackgroundColor(Color.RED)
+            holder.itemView.itemCatImageView.visibility = View.GONE
+            holder.itemView.undoDeleteCatButton.visibility = View.VISIBLE
+            holder.itemView.undoDeleteCatButton.setOnClickListener {
+                // user wants to undo the removal, let's cancel the pending task
+                val pendingRemovalRunnable = pendingRunnables[cat]
+                pendingRunnables.remove(cat)
+                if (pendingRemovalRunnable != null) handler.removeCallbacks(pendingRemovalRunnable)
+                itemsPendingRemoval.remove(cat)
+                // this will rebind the row in normal state
+                notifyItemChanged(catList.indexOf(cat))
+            }
+        }else {
+            // Show itemView in "normal" state
+            holder.itemView.apply {
+                setBackgroundColor(Color.WHITE)
+                itemCatImageView.visibility = View.VISIBLE
+                undoDeleteCatButton.visibility = View.GONE
+                undoDeleteCatButton.setOnClickListener(null)
+            }
             holder.bind(cat)
         }
     }
@@ -42,10 +80,37 @@ class FavoriteCatsAdapter(val onCatClicked: ((Cat) -> Unit)): RecyclerView.Adapt
     fun updateData(newCatList: List<Cat>) {
         catList = newCatList
     }
-    class CatViewHolder( itemView: View): RecyclerView.ViewHolder(itemView) {
-        companion object{
-            val requestOptions = RequestOptions().centerCrop()
+    fun pendingRemoval(position: Int) {
+        val cat: Cat = catList[position]
+        if (!itemsPendingRemoval.contains(cat)) {
+            itemsPendingRemoval.add(cat)
+            // this will redraw row in "undo" state
+            notifyItemChanged(position)
+            // let's create, store and post a runnable to remove the item
+            val pendingRemovalRunnable = Runnable { remove(catList.indexOf(cat)) }
+            handler.postDelayed(pendingRemovalRunnable, PENDING_REMOVAL_TIMEOUT)
+            pendingRunnables[cat] = pendingRemovalRunnable
         }
+    }
+//
+    fun remove(position: Int) {
+        val cat: Cat = catList.get(position)
+        if (itemsPendingRemoval.contains(cat)) {
+            itemsPendingRemoval.remove(cat)
+        }
+        if (catList.contains(cat)) {
+            onCatRemoved(cat)
+        }
+    }
+
+    fun isPendingRemoval(position: Int): Boolean {
+        val cat: Cat = catList.get(position)
+        return itemsPendingRemoval.contains(cat)
+    }
+
+    class CatViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val requestOptions = RequestOptions().centerCrop()
+
         fun bind(cat: Cat) {
 
             // Load images using Glide library
@@ -58,6 +123,5 @@ class FavoriteCatsAdapter(val onCatClicked: ((Cat) -> Unit)): RecyclerView.Adapt
                 .thumbnail()
                 .into(itemView.itemCatImageView)
         }
-
     }
 }
